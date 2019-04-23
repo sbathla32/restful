5344f36768b19f7590c4e151a8ef6508ff4afc55
sbathla32 <sahil.bathla20@gmail.com>
2019-04-23 07:42:54 +0530

    pending commit

---
+++ spring/src/main/java/com/rest/model/Employee.java
@@ -0,0 +1,25 @@
+package com.rest.model;
+
+public class Employee {
+	private String name;
+	private Integer id;
+	private String team;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getTeam() {
+		return team;
+	}
+	public void setTeam(String team) {
+		this.team = team;
+	}
+}
